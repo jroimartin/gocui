9b83f88bd3a4c73b0f1d7619e6af2099f0b83aae
Roi Martin (@nibble_ds) <jroi.martin@gmail.com>
2014-01-19 17:23:11 +0100

    Add example to documentation

---
+++ doc.go
@@ -0,0 +1,36 @@
+/*
+Package gocui allows to create console user interfaces.
+
+Example:
+
+	func layout(g *gocui.Gui) error {
+		maxX, maxY := g.Size()
+		if v, err := g.SetView("center", maxX/2-10, maxY/2, maxX/2+10, maxY/2+2); err != nil {
+			if err != gocui.ErrorUnkView {
+				return err
+			}
+			fmt.Fprintln(v, "This is an example")
+		}
+		return nil
+	}
+	func quit(g *gocui.Gui, v *gocui.View) error {
+		return gocui.ErrorQuit
+	}
+	func main() {
+		var err error
+		g := gocui.NewGui()
+		if err := g.Init(); err != nil {
+			log.Panicln(err)
+		}
+		defer g.Close()
+		g.SetLayout(layout)
+		if err := g.SetKeybinding("", gocui.KeyCtrlC, 0, quit); err != nil {
+			log.Panicln(err)
+		}
+		err = g.MainLoop()
+		if err != nil && err != gocui.ErrorQuit {
+			log.Panicln(err)
+		}
+	}
+*/
+package gocui
