479fc410af1430a59bbca3ba81a6bf1495b3ddf8
William <williamyeah@vip.qq.com>
2015-11-12 12:50:27 +0800

    support CJK(chinese, japanese, Korean)

--- view.go
+++ view.go
@@ -10,6 +10,8 @@ import (
 	"io"
 	"strings"
 
+	"github.com/mattn/go-runewidth"
+
 	"github.com/nsf/termbox-go"
 )
 
@@ -260,7 +262,7 @@ func (v *View) draw() error {
 			if err := v.setRune(x, y, ch); err != nil {
 				return err
 			}
-			x++
+			x += runewidth.RuneWidth(ch)
 		}
 		y++
 	}
