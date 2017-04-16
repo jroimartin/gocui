d47fadbf4a1f5df2088cdeb5646d352d7d2055c5
telecoda <robbaines@gmail.com>
2017-04-17 00:57:09 +0100

    Check for collisions in reverse view order

--- gui.go
+++ gui.go
@@ -182,7 +182,10 @@ func (g *Gui) View(name string) (*View, error) {
 // ViewByPosition returns a pointer to a view matching the given position, or
 // error ErrUnknownView if a view in that position does not exist.
 func (g *Gui) ViewByPosition(x, y int) (*View, error) {
-	for _, v := range g.views {
+
+	// traverse views in reverse order checking top views first
+	for i := len(g.views) - 1; i > 0; i-- {
+		v := g.views[i]
 		if x > v.x0 && x < v.x1 && y > v.y0 && y < v.y1 {
 			return v, nil
 		}
