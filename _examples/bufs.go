a4d0b30476148486f4c06df0f3742e359473618b
Roi Martin <jroi.martin@gmail.com>
2017-08-18 10:15:20 +0200

    Fix warning in _examples/bufs.go

--- _examples/bufs.go
+++ _examples/bufs.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// WARNING: tricky code just for testing purposes, do not use as example.
+// WARNING: tricky code just for testing purposes, do not use as reference.
 
 package main
 
