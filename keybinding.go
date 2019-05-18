68c8f545361763d41ea0dc3be71e5b5140a84a37
mjarkk <mkopenga@gmail.com>
2019-05-18 22:29:40 +0200

    Fixed a sugestion

--- keybinding.go
+++ keybinding.go
@@ -35,10 +35,7 @@ func (kb *keybinding) matchKeypress(key Key, ch rune, mod Modifier) bool {
 // matchView returns if the keybinding matches the current view.
 func (kb *keybinding) matchView(v *View) bool {
 	// if the user is typing in a field, ignore char keys
-	if v == nil {
-		return false
-	}
-	if v.Editable && kb.ch != 0 {
+	if v == nil || (v.Editable && kb.ch != 0) {
 		return false
 	}
 	return kb.viewName == v.name
