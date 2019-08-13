ff771a76f6cd783276449f59234152072c7101b8
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2019-08-14 00:46:21 +0900

    Fix build on Windows

--- gui.go
+++ gui.go
@@ -6,11 +6,7 @@ package gocui
 
 import (
 	standardErrors "errors"
-	"os"
-	"os/signal"
 	"runtime"
-	"syscall"
-	"unsafe"
 
 	"github.com/go-errors/errors"
 
@@ -762,47 +758,4 @@ func IsUnknownView(err error) bool {
 // IsQuit return true if the contents of an error is "quit"
 func IsQuit(err error) bool {
 	return err.Error() == ErrQuit.Error()
-}
-
-// getTermWindowSize is get terminal window size on linux or unix.
-// When gocui run inside the docker contaienr need to check and get the window size.
-func (g *Gui) getTermWindowSize() (int, int, error) {
-	var sz struct {
-		rows uint16
-		cols uint16
-	}
-
-	var termw, termh int
-
-	out, err := os.OpenFile("/dev/tty", os.O_RDWR, 0)
-	if err != nil {
-		return 0, 0, err
-	}
-	defer out.Close()
-
-	signalCh := make(chan os.Signal, 1)
-	signal.Notify(signalCh, syscall.SIGWINCH, syscall.SIGINT)
-
-	for {
-		_, _, _ = syscall.Syscall(syscall.SYS_IOCTL,
-			out.Fd(), uintptr(syscall.TIOCGWINSZ), uintptr(unsafe.Pointer(&sz)))
-
-		// check terminal window size
-		termw, termh = int(sz.cols), int(sz.rows)
-		if termw > 0 && termh > 0 {
-			return termw, termh, nil
-		}
-
-		select {
-		case signal := <-signalCh:
-			switch signal {
-			// when the terminal window size is changed
-			case syscall.SIGWINCH:
-				continue
-			// ctrl + c to cancel
-			case syscall.SIGINT:
-				return 0, 0, errors.New("stop to get term window size")
-			}
-		}
-	}
 }
